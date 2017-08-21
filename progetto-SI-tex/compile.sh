9b3dd8a72bf8fc54ab236e32397c6ee4ca748da6
synchro-- <alessio.salman@gmail.com>
2017-08-21 20:23:01 +0200

    Done with section 1.3 Data e Architettura

--- progetto-SI-tex/compile.sh
+++ progetto-SI-tex/compile.sh
@@ -1,6 +1,7 @@
 biber main
 pdflatex -synctex=1 -interaction=nonstopmode %.tex 
 biber main 
+biber main
 ppdflatex -synctex=1 -interaction=nonstopmode %.tex
 ppdflatex -synctex=1 -interaction=nonstopmode %.tex 
 
